5f86c81a56f563cbfccb85be4900bc71a3867a7e
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-03-19 19:51:03 -0700

    Extract method

---
+++ catalog_of_refactorings.rb
@@ -0,0 +1,21 @@
+# Extract method
+
+# before
+
+def print_owing(amount)
+  print_banner
+  puts "name: #{@name}"
+  puts "amount: #{amount}"
+end
+
+# after
+
+def print_owing(amount)
+  print_banner
+  print_details(amount)
+end
+
+def print_details(amount)
+  puts "name: #{@name}"
+  puts "amount: #{amount}"
+end
