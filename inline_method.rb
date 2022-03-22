6eabe83b65938c03df8b6172d3d1b187bd8ea00a
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-03-21 19:36:38 -0700

    inline tem exercise

---
+++ inline_method.rb
@@ -0,0 +1,20 @@
+# Extract method
+
+# before
+
+def get_rating
+  more_than_five_late_deliveries ? 2 : 1
+end
+
+
+def more_than_five_late_deliveries
+  @number_of_late_deliveries > 5
+end
+
+
+# after
+
+def get_rating
+  @number_of_late_deliveries > 5 ? 2 : 1
+end
+
