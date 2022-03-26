ddfcee95ea061882d9baa12d3a2cf58fb76750ad
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-03-25 21:57:38 -0700

    another explainable variable

--- explaining_variable.rb
+++ explaining_variable.rb
@@ -25,11 +25,23 @@ end
 
 # before
 
+def price
+  # price is base price - quantity discount + shipping
+  return @quantity * @item_price -
+    [0, @quantity - 500].max * @item_price * 0.05 +
+    [@quantity * @item_price * 0.1, 100.0].min
+end
 
 
 # after
 
-
+def price
+  # price is base price - quantity discount + shipping
+   base_price = @quantity * @item_price
+   quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
+   shipping = [base_price * 0.1, 100.0].min
+  return base_price - quantity_discount + shipping
+end
 
 
 
