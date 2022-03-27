3e273b64eaad41cf29fd9f7d36b02d2cd92f196f
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-03-26 21:43:28 -0700

    another example of Extract method

--- explaining_variable.rb
+++ explaining_variable.rb
@@ -43,11 +43,36 @@ def price
   return base_price - quantity_discount + shipping
 end
 
+# same example with Extract method
 
 
+# before
+
+def price
+  # price is base price - quantity discount + shipping
+  return @quantity * @item_price -
+    [0, @quantity - 500].max * @item_price * 0.05 +
+    [@quantity * @item_price * 0.1, 100.0].min
+end
 
 
+# after
 
+def price
+  base_price - quantity_discount + shipping
+end
+
+def base_price
+  @quantity * @item_price
+end
+
+def quantity_discount
+  [0, @quantity - 500].max * @item_price * 0.05
+end
+
+def shipping
+  [base_price * 0.1, 100.0].min
+end
 
 
 
