d04596d22c618d42eff24f1437bd7012f828311a
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-03-27 12:50:35 -0700

    split temp variable

---
+++ split_temp_var.rb
@@ -0,0 +1,31 @@
+# split temp var
+
+# before
+
+temp = 2 * (@height + @width)
+puts temp
+temp = @height + @width
+puts temp
+
+# after
+
+perimeter = 2 * (@height + @width)
+puts perimeter
+area = @height + @width
+puts area
+
+
+# another split temp var example
+
+# before
+
+
+
+# after
+
+
+
+
+
+
+
