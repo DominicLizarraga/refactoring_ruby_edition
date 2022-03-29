338f5c183bc41ea951f555a0b3799259e27686f0
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-03-28 17:24:46 -0700

    another example of split variable

--- split_temp_var.rb
+++ split_temp_var.rb
@@ -19,11 +19,34 @@ puts area
 
 # before
 
+def distance_traveled(time)
+  acc = @primary_force / @mass
+  primary_time = [time, @delay].min
+  result = 0.5 * acc * primary_time * primary_time
+  secondary_time = time - @delay
+  if (secondary_time > 0)
+    primary_vel = acc * @delay
+    acc = (@primary_force + @secondary_force) / @mass
+    result += primary_vel * secondary_time + 5 * acc * secondary_time * secondary_time
+  end
+  result
+end
 
 
 # after
 
-
+def distance_traveled(time)
+  primary_acc = @primary_force / @mass
+  primary_time = [time, @delay].min
+  result = 0.5 * acc * primary_time * primary_time
+  secondary_time = time - @delay
+  if (secondary_time > 0)
+    primary_vel = primary_acc * @delay
+    secondary_acc = (@primary_force + @secondary_force) / @mass
+    result += primary_vel * secondary_time + 5 * secondary_acc * secondary_time * secondary_time
+  end
+  result
+end
 
 
 
