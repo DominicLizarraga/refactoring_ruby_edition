0933969062f693156b5ad6fa85c14b924b98c227
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-03-30 21:06:48 -0700

    another example for assig and params

--- remove_assignment_to_params.rb
+++ remove_assignment_to_params.rb
@@ -2,7 +2,7 @@
 
 # before
 
-def discount(input_val, quantuty, year_to_date)
+def discount(input_val, quantity, year_to_date)
   if input_val > 50
     input_val -= 2
   end
@@ -15,11 +15,31 @@ end
 
 
 
-def discount(input_val, quantuty, year_to_date)
+def discount(input_val, quantity, year_to_date)
   result = input_val
   if input_val > 50
     result -= 2
   end
+end
+
+# before
+
+def discount(input_val, quantity, year_to_date)
+  input_val -= 2 if input_val > 50
+  input_val -= 1 if input_val > 100
+  input_val -= 4 if input_val > 1000
+  input_val
+end
+
+
+# after
+
+def dicount(input_val, quantity, year_to_date)
+  result = input_val
+  result -= 2 if input_val > 50
+  result -= 1 if input_val > 100
+  result -= 4 if input_val > 1000
+  result
 end
 
 
