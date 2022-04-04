13c9351b0dc4375a4386c47b242c6f8d3980544f
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-04-03 23:16:55 -0700

    conditional with refactoring

--- refactoring_on_conditional.rb
+++ refactoring_on_conditional.rb
@@ -12,19 +12,33 @@ end
 # after
 
 if not_summer?
-  charge = winter_rate quantity
+  charge = winter_charge quantity
 else
-  charge = summer_rate quantity
+  charge = summer_charge quantity
 end
 
 def not_summer?
   date < SUMMER_START || date > SUMMER_END
 end
 
-def winter_rate quantity
+def winter_charge quantity
   quantity * @winter_rate + @winter_service_charge
 end
 
-def summer_rate quantity
+def summer_charge quantity
   quantity * @summer_rate
 end
+
+
+
+
+# before
+
+parameters = params ? params : []
+
+# after
+
+parameters = params || []
+
+
+
