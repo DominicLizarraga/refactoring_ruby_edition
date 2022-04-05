9f6317c8a66c54473f514ae692245cf3ba7cbdd3
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-04-04 19:40:15 -0700

    consolidate conditionals

--- consolidate_conditionals.rb
+++ consolidate_conditionals.rb
@@ -19,12 +19,21 @@ def ineligable_for_disability?
   @senority < 2 || @months_disabled > 12 || @is_part_time
 end
 
-# before
 
+# example with ANDS
+
+# before
+if on_vacation?
+  if length_of_service > 10
+    return 1
+  end
+end
 
 # after
 
-
+if on_vacation? && length_of_service > 10
+  return 1
+end
 
 
 
