8cbc7c84da66e77b3204cd032c01ddb56f44a4d3
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-04-08 12:53:57 -0700

    replace loop chain method

--- substitute_loop.rb
+++ substitute_loop.rb
@@ -10,3 +10,15 @@ end
 # after
 
 managers = employees.select { |e| if e.manager? }
+
+# before
+
+managersOffices = []
+
+employees.each do |e|
+  managersOffices << e.office if e.manager?
+end
+
+# after
+
+managersOffices = employees.select { |e| if e.manager? }.collect { |e| e.office }
