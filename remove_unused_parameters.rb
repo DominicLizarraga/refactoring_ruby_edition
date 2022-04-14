1bd1a1659c5c28432f2804d2cc3832d41b7eb8fd
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-04-13 21:32:24 -0700

    typo

--- remove_unused_parameters.rb
+++ remove_unused_parameters.rb
@@ -3,7 +3,7 @@
 # before
 
 def product_count_items(search_criteria=nil)
-  criteria = search_criteria | @search_criteria
+  criteria = search_criteria || @search_criteria
   ProductCountItem.find_all_by_criteria(criteria)
 end
 
