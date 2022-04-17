5e584887d0ccf0d9751eefe735197445dd013662
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-04-16 19:48:11 -0700

    move field with self encapsulation

--- move_field.rb
+++ move_field.rb
@@ -1,4 +1,4 @@
-# move method
+# move field
 
 # before
 
@@ -25,3 +25,44 @@ class AccountType
     @account_type.interest_rate * amount * days / 365
   end
 end
+
+
+
+# example using self encapsulation
+
+
+# before
+
+class Account
+
+  def interest_for_amount_days(amount, days)
+    @interest_rate * amount * days / 365
+  end
+
+end
+
+# after
+
+class Account
+
+  attr_accessor :interest_rate
+
+  def interest_for_amount_days(amount, days)
+    interest_rate * amount * days / 365
+  end
+
+  private
+
+  def interest_rate
+    @account_type.interest_rate
+  end
+
+end
+
+
+
+
+
+
+
+
