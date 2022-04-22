2cecda8cd235a6a4c93d0b49c7a5228ed55ef4ba
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-04-21 21:38:52 -0700

    another example self encapsulation

--- self_encapsulate_field.rb
+++ self_encapsulate_field.rb
@@ -14,3 +14,47 @@ attr_reader :base_price, :tax_rate
 def total
   @base_price * (1 + @tax_rate)
 end
+
+
+# another example when having a subclass
+
+
+class Item
+
+  attr_reader :base_price, :tax_rate
+
+
+  def initialize(base_price, tax_rate)
+    setup(base_price, tax_rate)
+  end
+
+  def setup(base_price, tax_rate)
+    @base_price = base_price
+    @tax_rate = tax_rate
+  end
+
+  def raise_base_price(percent)
+    @base_price = @base_price * (1 + percent/100)
+  end
+
+  def total
+    @base_price * (1 + @tax_rate)
+  end
+
+end
+
+
+class ImportedItem < Item
+
+  attr_reader :import_duty
+
+  def initialize(base_price, tax_rate, import_duty)
+    super(base_price, tax_rate)
+    @import_duty = import_duty
+  end
+
+  def tax_rate
+    super + import_duty
+  end
+
+end
