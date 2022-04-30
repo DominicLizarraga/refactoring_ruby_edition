7090a490623a4fd7e1a6205b7241183ddbb4c736
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-04-30 11:10:11 -0700

    example with instance variable defined

--- lazily_initialized_attribute.rb
+++ lazily_initialized_attribute.rb
@@ -3,15 +3,47 @@
 # before
 
 class Employee
+
   def initialize
     @emails = []
   end
 
 end
+
 # after
 
-class Employee
+class Person
   def emails
     @emails ||= []
   end
 end
+
+
+# example using instance_variable_defined?
+
+# before
+
+class Employee
+
+  def initialize
+    @assistant = Employee.find_by_boss(self.id)
+  end
+
+end
+
+
+# after
+
+class Employee
+  def assistant
+    unless instance_variable_defined? :@assistant
+      @assistant = Employee.find_by_boss(self.id)
+    end
+    @assistant
+  end
+
+end
+
+
+
+
