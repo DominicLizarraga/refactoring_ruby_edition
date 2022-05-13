ef73c74e90905b50372696e33457e79ade197d67
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-05-12 21:11:47 -0700

    first classes/example of the book

---
+++ the_starting_point.rb
@@ -0,0 +1,44 @@
+# this is the first glance the book gives you of what refactoring really means
+
+# before
+
+
+class Movie
+
+  REGULAR = 0
+  NEW_RELEASE = 1
+  CHILDREN = 2
+
+  attr_reader :title
+  attr_accessor :price_code
+
+  def initialize(title, price_code)
+    @title, @price_code = title, price_code
+  end
+
+end
+
+class Rental
+  attr_reader :movie, :days_rented
+
+  def initialize(movie, days_rented)
+    @movie = movie
+    @days_rented = days_rented
+  end
+
+end
+
+class Customer
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @rentals = []
+  end
+
+  def add_rental(arg)
+    @rentals << arg
+  end
+
+
+end
